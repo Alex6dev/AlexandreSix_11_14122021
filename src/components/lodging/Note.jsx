2e119e88ed6b8b4fa0eb71c2ee183722503520d2
Alexandre Six <alexandresix2@gmail.com>
2022-01-02 14:02:54 +0100

    prêt pour soutenance

---
+++ src/components/lodging/Note.jsx
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+import starOff from '../../assets/starOff.png';
+import starOn from '../../assets/starOn.png';
+import './Note.css';
+
+
+class Note extends Component {
+  render(){
+    
+    const counter=[1,2,3,4,5]
+    
+
+    return ( 
+        <div>
+            
+        {counter.map((elt)=>this.props.rating>=elt ?(< img key={`${this.props.rating}-${elt}`}className='note' src={starOn} />  ):(<img className='note' key={`${this.props.rating}-${elt}`} src={starOff}/>))}
+        </div>
+    );
+  }
+
+}
+
+export default Note;
+/*
+
+*/
